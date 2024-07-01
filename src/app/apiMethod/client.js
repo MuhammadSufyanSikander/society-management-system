4d245dc66f101f56df3dd4a857876d37ed6a091b
Dilawer Hussain <dilawer.dev@gmail.com>
2024-07-01 10:40:15 -0700

    API fixed.

--- src/app/apiMethod/client.js
+++ src/app/apiMethod/client.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const production = 'https://society-management-system-delta.vercel.app/api'
 const local = 'http://localhost:3000/api'
 
-const isProduction = false
+const isProduction = true
 export const API_URL = isProduction ? production : local
 
 export default axios.create({
